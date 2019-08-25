0a15c62afa515c263fc58497cdb199d5acd15106
Robenter <mtpleasantgangsta@gmail.com>
2019-08-25 10:52:47 -0400

    Updated pages to use inidivual html pages for each game.  Added click events for logo and jump in button.  Removed apex folder from pages

--- webpage/pages/bfv/bfv.js
+++ webpage/pages/bfv/bfv.js
@@ -1,8 +1,4 @@
 function initializeBFVPage () {
-  $('.sym-main-content').html(
-        "<div style='text-align: center; padding: 150px; margin:auto; width: 400px;'>" +
-            '<h1>Battlefield V Stats</h1>' +
-            '<h4>Soon &trade;</h4>' +
-        '</div>'
-    )
+  $(".sym-main-content").empty()
+  $(".sym-main-content").load('./pages/bfv/bfv.html')
 }
