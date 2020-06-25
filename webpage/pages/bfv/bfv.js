4b2caa3a7d71f01f0381695aaea9400fe24e3933
Anssi "Miffyli" Kanervisto <kaneran21@hotmail.com>
2020-06-26 00:45:26 +0300

    Update BFV data

--- webpage/pages/bfv/bfv.js
+++ webpage/pages/bfv/bfv.js
@@ -1,10 +1,10 @@
 // Path to datafile
-const BFV_DATA = './pages/bfv/data/bfv_N.json'
+const BFV_DATA = './pages/bfv/data/bfv_O.json'
 
 // Manual dates when the data or pages have been modified.
 // In format "[day] [month three letters] [year four digits]"
 // e.g. 2nd Jan 2019
-const BFV_DATA_DATE = '7th Jun 2020 (BFV_N)'
+const BFV_DATA_DATE = '26th Jun 2020 (BFV_O)'
 const BFV_PAGE_DATE = '15th Mar 2020'
 
 // Total version string displayed under title
